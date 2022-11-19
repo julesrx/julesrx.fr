9dd4330ce3bf1965b61720ea33da3c3b426270f2
julesrx <25800503+julesrx@users.noreply.github.com>
2022-11-19 20:46:15 +0000

    Deploying to gh-pages from @ julesrx/julesrx@964c2b94ec80d8209f1e1e0abd8ed8b3484dbe39 🚀

---
+++ _nuxt/Markdown.4fd46707.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.99e7796a.js";import{f as o,h as f,p,B as u}from"./entry.44dca926.js";import"./utils.25a980bd.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=u(),{between:n,default:a}=f(),r=p(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
